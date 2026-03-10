4af7ba2311aabf57a5b923f6509ee14ce29f500b
penyelamatbumi <inihanip7@gmail.com>
2026-03-10 23:40:01 +0800

    menambahkan library

--- file1.cpp
+++ file1.cpp
@@ -30,5 +30,20 @@ string StatusBMI(float bmi)
         return "Obesitas";
 }
 
+int main()
+{
+    float berat, tinggi, bmi;
+
+    InputData(berat, tinggi);
+
+    bmi = HitungBMI(berat, tinggi);
+
+    cout << endl;
+    cout << "--- Hasil ---" << endl;
+    cout << "BMI Anda : " << bmi << endl;
+    cout << "Status   : " << StatusBMI(bmi) << endl;
+
+    return 0;
+}
 
 
