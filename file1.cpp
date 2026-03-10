0439d5a982f207bdbb75963384dca9d38f5656c6
penyelamatbumi <inihanip7@gmail.com>
2026-03-10 23:38:45 +0800

    menambahkan namespace

--- file1.cpp
+++ file1.cpp
@@ -11,3 +11,24 @@ void InputData(float &berat, float &tinggi)
     cin >> tinggi;
 }
 
+// Fungsi menghitung BMI
+float HitungBMI(float berat, float tinggi)
+{
+    return berat / (tinggi * tinggi);
+}
+
+// Fungsi menentukan status BMI
+string StatusBMI(float bmi)
+{
+    if (bmi < 18.5)
+        return "Berat Badan Kurang";
+    else if (bmi < 25)
+        return "Berat Badan Normal";
+    else if (bmi < 30)
+        return "Berat Badan Kelebihan";
+    else
+        return "Obesitas";
+}
+
+
+
